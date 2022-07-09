88b6582ba0f1d446efedb328d6bc0b79d7ebb7f5
Matheus Zufi Leite <matheuszufi@gmail.com>
2022-07-08 23:26:35 -0300

    UP

--- script/menuDown.js
+++ script/menuDown.js
@@ -2,3 +2,11 @@ const divMenuDown = document.getElementById("div-menu-down");
 const menuDown = document.getElementById("menu-down");
 
 
+const menuDownLoot = document.createElement('div');
+menuDownLoot.setAttribute('id', "menu-down-loot");
+menuDown.appendChild(menuDownLoot);
+
+const menuDownLootH1 = document.createElement('div');
+menuDownLootH1.setAttribute('id', "menu-down-loot-h1");
+menuDownLoot.appendChild(menuDownLootH1);
+menuDownLootH1.innerHTML = "COINS"
